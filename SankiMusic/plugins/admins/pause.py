866423c600f2a832478fca979ef1f9d9c15c8b9a
Aditya Halder <116054200+XdityaHalder@users.noreply.github.com>
2023-06-06 08:29:27 +0530

    Update pause.py

--- SankiMusic/plugins/admins/pause.py
+++ SankiMusic/plugins/admins/pause.py
@@ -27,7 +27,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Anon.pause_stream(chat_id)
+    await Kaal.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.first_name),
         reply_markup=close_keyboard
